74a4d7e659d2344ee890568d5ff1ffd9049d0a2e
M. Morgan Aster <marymorg@broadinstitute.org>
2024-11-05 17:18:53 -0500

    TSPS-363 fix: move config file so it's included in package (#8)

--- terralab/config.py
+++ terralab/config.py
@@ -1,15 +1,20 @@
 # config.py
 
+import logging
 from pathlib import Path
 from oauth2_cli_auth import OAuth2ClientInfo
 from dotenv import dotenv_values
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 class CliConfig:
     """A class to hold configuration information for the CLI"""
 
-    def __init__(self, config_file="../.terralab-cli-config"):
+    def __init__(self, config_file="terralab/.terralab-cli-config"):
         self.config = dotenv_values(config_file)
+        LOGGER.debug(f"Found config with values: {self.config}")
 
         self.client_info = OAuth2ClientInfo.from_oidc_endpoint(
             self.config["OAUTH_OPENID_CONFIGURATION_URI"],
