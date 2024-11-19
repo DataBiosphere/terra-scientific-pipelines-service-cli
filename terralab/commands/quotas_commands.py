80397a3494502b99921045aeb3a8b2c35b7ed223
Jose Soto <jsoto@broadinstitute.org>
2024-11-19 17:41:03 -0500

    first commit, lets try it!

---
+++ terralab/commands/quotas_commands.py
@@ -0,0 +1,31 @@
+# commands/quotas_commands.py
+
+import logging
+
+import click
+
+from terralab.log import indented
+from terralab.logic import quotas_logic
+from terralab.utils import handle_api_exceptions
+
+LOGGER = logging.getLogger(__name__)
+
+
+@click.group()
+def quotas():
+    """Get information about user quota"""
+
+
+@quotas.command()
+@click.argument("pipeline_name")
+@handle_api_exceptions
+def get_info(pipeline_name: str):
+    """Get user quota information for a specific pipeline"""
+    quota_info = quotas_logic.get_user_quota(pipeline_name)
+    quota = quota_info.quota_limit
+    quota_consumed = quota_info.quota_consumed
+    quota_pipeline = quota_info.pipeline_name
+    LOGGER.info(f"Pipeline: {quota_pipeline}")
+    LOGGER.info(indented(f"Quota Limit: {quota}"))
+    LOGGER.info(indented(f"Quota Used: {quota_consumed}"))
+    LOGGER.info(indented(f"Quota Left: {quota - quota_consumed}"))
---
+++ tests/commands/test_quotas_commands.py
@@ -0,0 +1,66 @@
+# tests/commands/test_quotas_commands.py
+
+from click.testing import CliRunner
+from mockito import when, verify
+from teaspoons_client import QuotaWithDetails, ApiException
+
+from terralab.commands import quotas_commands
+from tests.utils_for_tests import capture_logs
+
+
+def test_get_info_success(capture_logs, unstub):
+    test_pipeline_name = "test_pipeline"
+    test_quota_details = QuotaWithDetails(
+        pipeline_name=test_pipeline_name, quota_limit=1000, quota_consumed=300
+    )
+
+    when(quotas_commands.quotas_logic).get_user_quota(test_pipeline_name).thenReturn(
+        test_quota_details
+    )
+
+    runner = CliRunner()
+    result = runner.invoke(quotas_commands.quotas, ["get-info", test_pipeline_name])
+
+    assert result.exit_code == 0
+    verify(quotas_commands.quotas_logic).get_user_quota(test_pipeline_name)
+    assert test_pipeline_name in capture_logs.text
+    # quota limit
+    assert "1000" in capture_logs.text
+    # quota consumed
+    assert "300" in capture_logs.text
+    # quota left
+    assert "700" in capture_logs.text
+
+    unstub()
+
+
+def test_get_info_missing_argument():
+    runner = CliRunner()
+
+    result = runner.invoke(quotas_commands.quotas, ["get-info"])
+
+    assert result.exit_code != 0
+    assert "Error: Missing argument 'PIPELINE_NAME'" in result.output
+
+
+def test_get_info_api_exception(capture_logs, unstub):
+    runner = CliRunner()
+
+    when(quotas_commands.quotas_logic).get_user_quota("bad_pipeline_name").thenRaise(
+        ApiException(
+            status=400,
+            reason="Error Reason",
+            body='{"message": "this is the body message"}',
+        )
+    )
+
+    result = runner.invoke(quotas_commands.quotas, ["get-info", "bad_pipeline_name"])
+
+    assert result.exit_code != 0
+    verify(quotas_commands.quotas_logic).get_user_quota("bad_pipeline_name")
+    assert (
+        "API call failed with status code 400 (Error Reason): this is the body message"
+        in capture_logs.text
+    )
+
+    unstub()
