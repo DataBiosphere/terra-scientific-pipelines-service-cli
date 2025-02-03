571ca952bb627beae53b5465e0029ca2ae40d109
M. Morgan Aster <marymorg@broadinstitute.org>
2025-02-03 11:33:57 -0500

    TSPS-419 Restrict permissions on token files (#36)

--- tests/test_auth_helper.py
+++ tests/test_auth_helper.py
@@ -2,6 +2,7 @@
 
 import builtins
 import logging
+import os
 
 import pytest
 from jwt import ExpiredSignatureError
@@ -528,10 +529,18 @@ def test_save_local_token(mock_builtin_open, unstub):
     mock_access_token_file = mock()
     mock_token = mock()
     mock_dirname = mock()
+    mock_descriptor = mock()
 
     when(auth_helper.os.path).dirname(mock_access_token_file).thenReturn(mock_dirname)
     when(auth_helper.os).makedirs(mock_dirname, exist_ok=True).thenReturn(None)
-    when(mock_builtin_open).write().thenReturn(None)
+    when(auth_helper.os).open(mock_access_token_file, os.O_WRONLY | os.O_CREAT, 0o600).thenReturn(mock_descriptor)
+    
+    mock_fdopen = mock()
+    when(mock_fdopen).__enter__().thenReturn(mock_fdopen)
+    when(mock_fdopen).__exit__(None, None, None).thenReturn(None)
+    when(auth_helper.os).fdopen(mock_descriptor, "w").thenReturn(mock_fdopen)
+
+    when(mock_fdopen).write().thenReturn(None)
 
     auth_helper._save_local_token(mock_access_token_file, mock_token)
 
