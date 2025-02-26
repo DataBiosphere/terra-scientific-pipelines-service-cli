a6fee1f70b2c6e2382704904e3509cb280281fcd
M. Morgan Aster <marymorg@broadinstitute.org>
2025-02-26 15:08:34 -0500

    TSPS-406 Add disclaimer about quota update time (#46)

--- terralab/commands/quotas_commands.py
+++ terralab/commands/quotas_commands.py
@@ -21,6 +21,10 @@ def quota(pipeline_name: str) -> None:
     quota_consumed = quota_info.quota_consumed
     quota_pipeline = quota_info.pipeline_name
     quota_units = quota_info.quota_units
+    
+    LOGGER.info(
+        "Note: It may take a few minutes for recently submitted jobs to be reflected."
+    )
     LOGGER.info(f"Pipeline: {quota_pipeline}")
     LOGGER.info(indented(f"Quota Limit: {quota_limit} {quota_units}"))
     LOGGER.info(indented(f"Quota Used: {quota_consumed} {quota_units}"))
