0f1b0539f52f7296b5f6425b3193a7322dfe4467
Matt Bemis <mbemis@broadinstitute.org>
2026-03-24 14:06:47 -0400

    go back to pulling email out of jwt

--- terralab/sam_helper.py
+++ terralab/sam_helper.py
@@ -1,43 +1,16 @@
 # sam_helper.py
 
-import json
 import logging
 from urllib import request as urllibrequest, error as urlliberror
+
+import jwt
 
 from terralab.config import CliConfig
 from terralab.constants import SUPPORT_EMAIL
 
 LOGGER = logging.getLogger(__name__)
 
-SELF_INFO_ENDPOINT = "/api/users/v2/self"
 PROXY_GROUP_ENDPOINT = "/api/google/v1/user/proxyGroup/{email}"
-
-
-def get_user_email(cli_config: CliConfig, access_token: str) -> str:
-    """Get the email address of the logged-in user from Sam.
-
-    :param cli_config: Configuration object containing the Sam API URL
-    :param access_token: A valid access token for the logged-in user
-    :return: The user's email address
-    """
-    url = f"{cli_config.sam_api_url}{SELF_INFO_ENDPOINT}"
-    LOGGER.debug("Fetching user info from Sam")
-
-    req = urllibrequest.Request(
-        url,
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-    try:
-        with urllibrequest.urlopen(req) as response:
-            data = json.loads(response.read().decode("utf-8"))
-            email: str = data["email"]
-            return email
-    except urlliberror.URLError as e:
-        LOGGER.debug(f"Failed to retrieve user info from Sam: {e}")
-        LOGGER.error(
-            f"Failed to retrieve user info, please try again or contact support at {SUPPORT_EMAIL}"
-        )
-        raise
 
 
 def get_user_proxy_group(cli_config: CliConfig, access_token: str) -> str:
@@ -47,7 +20,7 @@ def get_user_proxy_group(cli_config: CliConfig, access_token: str) -> str:
     :param access_token: A valid access token for the logged-in user
     :return: The proxy group email address
     """
-    email = get_user_email(cli_config, access_token)
+    email = _get_email_from_token(access_token)
     url = f"{cli_config.sam_api_url}{PROXY_GROUP_ENDPOINT.format(email=email)}"
     LOGGER.debug(f"Fetching proxy group from Sam for user {email}")
 
@@ -65,3 +38,14 @@ def get_user_proxy_group(cli_config: CliConfig, access_token: str) -> str:
             f"Failed to retrieve user info, please try again or contact support at {SUPPORT_EMAIL}"
         )
         raise
+
+
+def _get_email_from_token(access_token: str) -> str:
+    """Decode the JWT access token and return the user's email address."""
+    # Note that we do not need to verify the JWT signature or expiration here, since we just need to
+    # extract the email claim. Wherever the token is used downstream by a service, it'll be verified by that service.
+    decoded = jwt.decode(
+        access_token, options={"verify_signature": False, "verify_exp": False}
+    )
+    email: str = decoded["email"]
+    return email
