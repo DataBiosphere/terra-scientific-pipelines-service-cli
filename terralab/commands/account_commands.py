51322685852ce118ecb61c4ff8ec28b7d3e3c5f9
Matt Bemis <mbemis@broadinstitute.org>
2026-03-24 17:18:40 -0400

    reformat output

--- terralab/commands/account_commands.py
+++ terralab/commands/account_commands.py
@@ -4,7 +4,7 @@ import logging
 
 import click
 
-from terralab.log import format_table_no_header
+from terralab.log import format_table_no_header, indented, add_blankline_before
 from terralab.logic import account_logic
 
 LOGGER = logging.getLogger(__name__)
@@ -12,14 +12,21 @@ LOGGER = logging.getLogger(__name__)
 
 @click.command(
     name="account",
-    short_help="Get information about sharing cloud resources with Broad Scientific Services",
+    short_help="Get information about your account",
 )
 def cloud_info() -> None:
     """Get cloud sharing information for your account"""
+    account_info = account_logic.get_account_info()
+    LOGGER.info("Your Account")
+    LOGGER.info(add_blankline_before(format_table_no_header(account_info)))
+
     rows = account_logic.get_cloud_info()
+    LOGGER.info(add_blankline_before("Cloud Resource Sharing"))
     LOGGER.info(
-        "To ensure that your cloud resources can be properly accessed by Broad Scientific Services, please share them with the following accounts:\n"
+        add_blankline_before(
+            "To ensure that your cloud resources can be properly accessed by Broad Scientific Services, please share them with the following accounts:"
+        )
     )
-    LOGGER.info(format_table_no_header(rows))
+    LOGGER.info(add_blankline_before(format_table_no_header(rows)))
     emails = ", ".join(row[1] for row in rows)
-    LOGGER.info(f"\nCopy all: {emails}")
+    LOGGER.info(indented(f"\nCopy all: {emails}"))
--- terralab/logic/account_logic.py
+++ terralab/logic/account_logic.py
@@ -2,11 +2,31 @@
 
 import logging
 
+import jwt
+
 from terralab.auth_helper import get_or_refresh_access_token
 from terralab.config import load_config
-from terralab.sam_helper import get_user_proxy_group
+from terralab.sam_helper import get_user_proxy_group, _get_email_from_token
 
 LOGGER = logging.getLogger(__name__)
+
+
+def get_account_info() -> list[list[str]]:
+    """Get basic account information for the logged-in user from the JWT.
+
+    :return: A list of rows to display in the account info table
+    """
+    config = load_config()
+    access_token = get_or_refresh_access_token(config)
+    decoded = jwt.decode(
+        access_token, options={"verify_signature": False, "verify_exp": False}
+    )
+    name = decoded.get("name", "")
+    email = _get_email_from_token(access_token)
+    return [
+        ["Name", name],
+        ["Email Address", email],
+    ]
 
 
 def get_cloud_info() -> list[list[str]]:
