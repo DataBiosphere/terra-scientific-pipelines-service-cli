7058f600e8f4e7a762f9da05f929cc8b6ccf2401
Matt Bemis <MatthewBemis@users.noreply.github.com>
2026-08-06 15:17:01 -0400

    Update constants.py

--- terralab/constants.py
+++ terralab/constants.py
@@ -18,7 +18,7 @@ GCS_PREFIX = "gs://"
 MAX_FILE_UPLOAD_SIZE_BYTES = 50 * 1025 * 1024 * 1024  # 50GB
 
 # support text
-SUPPORT_EMAIL = "scientific-services-support@broadinstitute.org"
+SUPPORT_EMAIL = "data-science-services-support@broadinstitute.org"
 SUPPORT_EMAIL_TEXT = f"For help troubleshooting, email {SUPPORT_EMAIL}"
 TERMS_OF_SERVICE_URL = "https://services.terra.bio/#pipelines/terms-of-service"
 QUOTAS_SUPPORT_ARTICLE_URL = (
