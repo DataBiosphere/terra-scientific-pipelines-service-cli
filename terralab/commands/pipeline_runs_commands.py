5fc348f097eb0c7eafb7fbd3eaca0c22171deed7
Morgan Taylor <marymorg@broadinstitute.org>
2026-09-09 14:59:39 -0400

    simplify output display

--- terralab/commands/pipeline_runs_commands.py
+++ terralab/commands/pipeline_runs_commands.py
@@ -193,27 +193,23 @@ def display_outputs(outputs: dict[str, Any] | None) -> None:
     ) in outputs.items():
         LOGGER.info(indented(f"{output_name}:"))
         if isinstance(output_value, list):
-            for i, item in enumerate(output_value):
-                display_single_output(item)
+            for item in output_value:
+                display_single_output_value(item)
         else:
-            display_single_output(output_value)
+            display_single_output_value(output_value)
 
 
-def display_single_output(output_value: Any) -> None:
-    LOGGER.info(
-        indented(
-            process_file_output_with_size_string(output_value),
-            n_spaces=4,
-        )
-    )
-
-
-def process_file_output_with_size_string(output_value: dict[str, Any]) -> str:
+def display_single_output_value(output_value: Any) -> None:
     if "metadata" in output_value and "sizeInBytes" in output_value["metadata"]:
         output_size_string = f"({convert_file_size_to_human_readable(output_value['metadata']['sizeInBytes'])})"
     else:
         output_size_string = ""
-    return f"{output_value['value']} {output_size_string}"
+    LOGGER.info(
+        indented(
+            f"{output_value['value']} {output_size_string}",
+            n_spaces=4,
+        )
+    )
 
 
 def display_data_delivery(data_delivery_report: DataDeliveryReport | None) -> None:
