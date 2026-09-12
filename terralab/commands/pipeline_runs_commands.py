2a530ca52a290e9031ae34874f11281a5f3d9bad
Morgan Taylor <marymorg@broadinstitute.org>
2026-09-11 21:13:00 -0400

    add function description

--- terralab/commands/pipeline_runs_commands.py
+++ terralab/commands/pipeline_runs_commands.py
@@ -224,6 +224,7 @@ def display_single_output_value(output_value: Any) -> None:
 
 
 def is_not_none(val: int | None) -> TypeGuard[int]:
+    """Check if the given value is not None, returning True if it is an int. Used as a TypeGuard for filtering out None values to guarantee that the resulting filtered list contains only ints."""
     return val is not None
 
 
